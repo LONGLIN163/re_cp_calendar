2b7e6e9d61f979988f67a24a084853983b6c5842
developerlin123 <developerlin123@gmail.com>
2021-04-28 17:46:24 +0200

    year h-scrollbar done

--- app/YearMonthPicker.js
+++ app/YearMonthPicker.js
@@ -14,10 +14,19 @@ class YearMonthPicker extends Component{
     }
 
     rrBtnHandler(currentYear){
-        $(this.refs.span_container).append($(`<span >${currentYear+3}</span>`)).stop(true).animate({"left":-38},600,function () {
-            $(this).find("span").eq(0).remove();
-            $(this).css({"left":0});
+        $(this.refs.span_container).append($(`<span >${currentYear+3}</span>`)).stop(true).animate({"left":-38},300,function () { 
+            $(this).find("span").eq(0).remove(); // remove the first one
+            $(this).css({"left":0}); //move back
             $(this).find("span").eq(1).removeClass("cur");
+            $(this).find("span").eq(2).addClass("cur");
+        })
+    }
+
+    llBtnHandler(currentYear){
+        $(this.refs.span_container).prepend($(`<span >${currentYear-3}</span>`)).stop(true).animate({"left":+38},300,function () { 
+            $(this).find("span").eq(-1).remove();//remove the last one
+            $(this).css({"left":0}); //move back
+            $(this).find("span").eq(3).removeClass("cur");
             $(this).find("span").eq(2).addClass("cur");
         })
     }
@@ -37,7 +46,7 @@ class YearMonthPicker extends Component{
                                 <span >{currentYear+2}</span>
                              </div>
                          </div>
-                         <i className="ll"></i>
+                         <i className="ll" onClick={()=>{this.llBtnHandler(currentYear--)}}></i>
                          <i className="rr" onClick={()=>{this.rrBtnHandler(currentYear++)}}></i>
                      </div>
                      <div className="month_panel">
